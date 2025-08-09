79b8541321ac20a4bfeac350b94fede328bbe88e
Shamarpryce <shamarpryce258@gmail.com>
2025-08-09 07:40:42 -0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,1 @@
-from flask import Flask  # type: ignore
-from threading import Thread
 
-app = Flask(__name__)  # use __name__ instead of '/'
-
-@app.route('/')
-def home():
-    return "Bot is running!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.daemon = True  # optional but good to let thread exit on main exit
-    t.start()
